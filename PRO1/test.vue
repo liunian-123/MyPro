bce3e4091b1e148843a4201ee6c0c4f058062e97
liunian-123 <1098415909@qq.com>
2021-06-17 16:01:25 +0800

    vue 3

---
+++ PRO1/test.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="app">
+    <p>页面载入时，input 元素自动获取焦点：</p>
+    <input v-focus>
+</div>
+</template>
+
+<script>
+const app = {
+   data() {
+      return {
+      }
+   },
+   directives: {
+      focus: {
+         // 指令的定义
+         mounted(el) {
+            el.focus()
+         }
+      }
+   }
+}
+ 
+Vue.createApp(app).mount('#app')
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
