6ddb713131d599f4f41313dd848b9249aba35a46
liunian-123 <1098415909@qq.com>
2021-06-18 17:21:42 +0800

    in order

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,25 +1,33 @@
 <template>
-  <div>
-
+  <div class="app">
+    <div>{{money}}</div>
+    <button @click="money++">修改</button>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-const MyComponent = defineComponent({
-  data () {
-    return { count: 1 }
-  },
-  methods: {
-    increment () {
-      this.count++
+import { ref } from 'vue'
+// reactive 函数接受一个普通对象，返回该普通对象的响应式对象
+export default {
+  setup () {
+    // ref 函数接受一个简单类型,返回一个响应式的对象
+    // 响应式对象只有一个属性value
+    // 在模板中使用ref 会自动调用value
+    const money = ref(100)
+    // money++   显示NAN  因为 money被ref转换成了一个对象  对象不能++
+    money.value++
+    return {
+      money
     }
+  },
+  beforeCreate () {
+    console.log('beforeCreate')
   }
-})
-
-console.log(`MyComponent:${MyComponent}`)
+}
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
+.app{
+  background-color: rebeccapurple;
+}
 </style>
