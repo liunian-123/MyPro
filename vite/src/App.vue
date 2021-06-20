90fbc169e02c9038c35cf00ec5a4970ce102f58e
liunian-123 <1098415909@qq.com>
2021-06-20 14:11:12 +0800

    emit

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -4,18 +4,26 @@
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
  <!-- toeleport -->
   <ModelButton></ModelButton>
+  <Emit @click="onClick"></Emit>
 </div>
 </template>
 
 <script>
 import ModelButton from './components/ModelButton.vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Emit from './components/Emit.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    ModelButton
+    ModelButton,
+    Emit
+  },
+  methods: {
+    onClick(){
+      console.log("click on app")
+    }
   }
 }
 </script>
---
+++ vite/src/components/Emit.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Emit">
+    <!-- 当自定义事件的名字不是click 可以不用emit -->
+    <div @click="$emit('click')">自定义事件</div>
+  </div>
+</template>
+<script>
+export default {
+ emits: ['click']
+}
+</script>
+
+<style scoped>
+</style>
