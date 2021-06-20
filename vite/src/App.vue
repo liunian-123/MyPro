b63493f9dd242d857b67861a4a8f879d9a69bb7b
liunian-123 <1098415909@qq.com>
2021-06-20 19:14:15 +0800

    vue3

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -1,27 +1,61 @@
 <template>
-<div class="app">
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
- <!-- toeleport -->
-  <ModelButton></ModelButton>
-  <Emit @click="onClick"></Emit>
-</div>
+  <div class="app">
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <!-- toeleport -->
+    <ModelButton></ModelButton>
+    <Emit @click="onClick"></Emit>
+    <!-- v-model 的使用 -->
+    <VmodelTest v-model="counter"></VmodelTest>
+    <!-- v-model:counter="counter"   :counter="counter" @update:counter = counter=$event -->
+    <!-- render api 变化 -->
+    <RenderTest v-model:counter="counter"></RenderTest>
+  </div>
 </template>
 
 <script>
+import { h } from 'vue'
 import ModelButton from './components/ModelButton.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import Emit from './components/Emit.vue'
+import VmodelTest from './components/VmodelTest.vue'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      counter: 1
+    }
+  },
   components: {
     HelloWorld,
     ModelButton,
-    Emit
+    Emit,
+    VmodelTest,
+    RenderTest: {
+      props: {
+        counter: {
+          type: Number,
+          default: 0
+        },
+      },
+      render () {
+        return h('div', [
+          h('div', {onClick: this.onClick}, `I am RenserTest:${this.counter}`)
+        ])
+      },
+      methods: {
+        onClick () {
+          this.$emit('update:counter',this.counter + 1)
+        }
+      }
+    }
   },
   methods: {
-    onClick(){
+    onClick () {
       console.log("click on app")
     }
   }
